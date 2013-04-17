e30f4ec272bb3301f78d0c8aee86126d420bd8a2
Samuel C. Tyler <skamansam@gmail.com>
2013-04-17 14:33:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DynamicRails::Application.config.secret_token = 'd05942a108ed415b7a38fea27327302ea5d78910fe9a1aecf4bc4906f1bd9344336e0b57aa78b2059ec05bb6d5b28611a4b4fd5aac903b7090b92bc3bc98bb50'
